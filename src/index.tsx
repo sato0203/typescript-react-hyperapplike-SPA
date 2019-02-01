5fa2db91d8b5d245d429c5e11163814d1c7e2668
佐藤輝年 <sato.geijo.13@gmail.com>
2019-02-01 10:58:30 +0900

    react-router追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import * as serviceWorker from './serviceWorker';
 import _states, { States } from "./states";
 import _actions, { Actions } from "./actions"
 import Count from "./views/Count"
+import {Route, BrowserRouter} from "react-router-dom"
+import "./index.css"
 
 const view = (state:States, actions:ComponentActions<States,Actions>) => (
-  <Count state={state} actions={actions}/>
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' render ={() => <Count state={state} actions={actions}/>} />
+    </div>
+  </BrowserRouter>
 );
 
 const element = document.getElementById('root');
